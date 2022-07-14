a11ee86073cabe9e9941e0ecbac2ccc64327573e
furkan <furkanesen35@gmail.com>
2022-07-14 16:30:20 +0200

    first commit

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CardStyle from "./Card.style"
+
+const Card = () => {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutStyle from "./About.style"
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
