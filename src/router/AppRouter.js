f4527c7947938aac41fac9184d3d9218e2b8e43a
furkan <furkanesen35@gmail.com>
2022-08-07 20:07:35 +0200

    routers done

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -5,6 +5,7 @@ import Navbar from '../components/navbar/Navbar'
 import Home from "../pages/home/Home"
 import Login from "../pages/login/Login"
 import About from "../pages/about/About"
+import PrivateRouter from "./PrivateRouter.jsx"
 
 const AppRouter = () => {
  return (
@@ -14,7 +15,9 @@ const AppRouter = () => {
    <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/login' element={<Login/>} />
-    <Route path='/about' element={<About/>} />
+    <Route path="/about" element={<PrivateRouter/>}>
+     <Route path='' element={<About/>} />
+    </Route>
    </Routes>
   </BrowserRouter>
  )
