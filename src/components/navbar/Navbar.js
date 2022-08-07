c30534b8a3ddfff862d1bd68aa372f4081044159
furkan <furkanesen35@gmail.com>
2022-08-07 23:23:39 +0200

    navbar done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom"
-import Nav, {} from "./Navbar.style"
+import Nav, {Logo,MenuLink,Menu,Hamburger } from "./Navbar.style"
+import {GiHamburgerMenu} from "react-icons/gi"
 
 const Navbar = () => {
+ const [isOpen, setIsOpen] = useState(false);
  return (
-  <nav>
-   <div>
+  <Nav justify="space-between" wrap="wrap">
+   <Logo to="/">
     <i>Find Your Flavor</i>
-   </div>
-   <div>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/login">Login</Link>
-    <Link to="/register">Register</Link>
-   </div>
-  </nav>
+   </Logo>
+   <Hamburger onClick={()=>setIsOpen(!isOpen)}>
+    <GiHamburgerMenu/>
+   </Hamburger>
+   <Menu isOpen={isOpen} onClick={()=>setIsOpen(false)}>
+    <MenuLink to="/">Home</MenuLink>
+    <MenuLink to="/about">About</MenuLink>
+    <MenuLink to="/login" onClick={()=>sessionStorage.clear()}>Logout</MenuLink>
+    <MenuLink to="/register">Register</MenuLink>
+   </Menu>
+  </Nav>
  )
 }
 
--- src/components/navbar/Navbar.style.js
+++ src/components/navbar/Navbar.style.js
@@ -1,9 +1,51 @@
 import styled from 'styled-components';
 import Flex from '../globalStyles/Flex';
+import {Link} from "react-router-dom"
 
 const Nav = styled(Flex)`
  padding: 1rem 2rem;
- background: ${({theme})=>theme.colors.navBgColor};
+ background: ${({ theme })=> theme.colors.navBgColor};
 `;
+
+export const Logo = styled(Link)`
+ padding: 1rem 2rem;
+ color: ${({ theme }) => theme.colors.logoColor};
+ text-decoration: none;
+ font-weight: 800;
+ font-size: 1.5rem;
+ span{
+  font-weight: 400;
+  color: ${({theme}) => theme.colors.mainColor};
+ }
+`
+export const MenuLink = styled(Link)`
+ text-align: center;
+ padding: 1rem 2rem;
+ cursor: pointer;
+ color: ${({ theme }) => theme.colors.linkColor};
+ text-decoration: none;
+ transition: all 0.3s ease-in;
+ &:hover{
+  font-weight: bold;
+  color: ${({theme}) => theme.colors.mainColor};
+ }
+`
+
+export const Menu = styled(Flex)`
+ @media (max-width: ${({theme}) => theme.screens.lg}){
+  flex-direction: column;
+  width: 100%;
+  max-height: ${({isOpen})=> (isOpen ? "400px" : "0")};
+  overflow: hidden;
+`
+
+export const Hamburger = styled.div`
+ display: none;
+ cursor: pointer;
+ color: ${({theme}) => theme.colors.mainColor};
+ @media (max-width: ${({theme}) => theme.screens.lg}){
+  display: block;
+ }
+`
 
 export default Nav;
