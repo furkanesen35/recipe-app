25923245d9d6d49df414673d44cca6891e830581
furkan <furkanesen35@gmail.com>
2022-08-08 05:01:27 +0200

    changes

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import defaultImage from "../../assets/default-image.jpg"
-import CardStyle,{Card, Header, MainContainer,Image, Button} from "./Card.style"
+import {Card, Header, MainContainer,Image, Button} from "./Card.style"
+import { useNavigate } from 'react-router-dom'
 
 const Cards = ({recipes}) => {
+ const navigate = useNavigate();
  return (
   <MainContainer wrap="wrap">
    {recipes.map(({recipe},index)=>(
      <Card key={index}>
       <Header>{recipe.label}</Header>
       <Image src={recipe.image || defaultImage} />
-      <Button>View more</Button>
+      <Button onClick={() => navigate("detail", {state:recipe,replace:false})} >View more</Button>
      </Card>
    ))}
   </MainContainer>
